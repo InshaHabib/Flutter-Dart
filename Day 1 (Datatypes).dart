59c9ba8f16e26a4b051d679386f876bee4cdd24a
Insha Habib <145709260+InshaHabib@users.noreply.github.com>
2024-07-17 22:59:24 +0500

    dartpad file

---
+++ Day 1 (Datatypes).dart
@@ -0,0 +1,27 @@
+// Datatypes
+
+void main(){
+  print(23.runtimeType);	
+  print(23.5.runtimeType);
+  print(4.isEven);
+  print('My name is inshahabib'.runtimeType);
+  num i=5;
+  print(i);
+  
+  // double
+  double a=8;
+  print(a);  
+  //int b=6.5;
+  // print(b); //Error occur
+
+// List
+ List c = [1,2,3,4,5,6,'str',true];
+  print(c);
+  print(c[7]);
+
+// Map
+  Map i = {"name":'insha', "serial No":90};
+  print(i["name"]);
+  print(i["serial No"]);
+}
+
---
+++ Day 2 (Operators, Variables).dart
@@ -0,0 +1,30 @@
+void main() {
+  // Arithmetic operators
+  int a = 5;
+  int b = 10;
+  int c = 2;
+  int d = 13;
+  int e = 4;
+  int f = 9;
+  int g = 45;
+
+  print(a / b);
+  print(c % d);
+  print(e * f);
+  print(g + b);
+  print(g - a);
+  
+  // Relational operators
+  print(a == a);
+  print(a != a);
+  print(g <= g);
+  print(a >= b);
+  
+  // Logical operators
+  print(a == a && b != 8);
+  print(b == b || c >= b);
+  
+  // Variables
+  int age = 10;
+  print(age);
+}
